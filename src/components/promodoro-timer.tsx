4909fb811be4539ea4453e0f3293e9dfe8bfcfd3
Daviniel Nascimento <eng.daviniel@gmail.com>
2023-11-03 17:54:44 -0300

    styled create

--- src/components/promodoro-timer.tsx
+++ src/components/promodoro-timer.tsx
@@ -11,15 +11,26 @@ interface Props {
 }
 
 export function PromodoroTimer(props: Props): JSX.Element {
+
   const [mainTime, setMainTime] = React.useState(props.promodoroTime);
   useInterval(() => {
     setMainTime(mainTime - 1);
   }, 1000);
+
   return (
     <div className="promodoro">
+
       <h2>You are: working</h2>
+
       <Timer mainTime={mainTime} />
-      <Button text="teste" onClick={() => console.log(1)}></Button>
+
+      <div className="controls">
+        <Button text="teste" onClick={() => console.log(1)}></Button>
+      </div>
+
+      <div className="details">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores molestiae omnis eaque perspiciatis error suscipit ipsa natus cumque ad! Voluptates tempora deserunt quam libero beatae distinctio nostrum obcaecati atque. Dolore.</p>
+      </div>
     </div>
   )
 }
