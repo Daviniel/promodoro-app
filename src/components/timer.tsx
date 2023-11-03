d14e761d01e8474f2c255ac7f19b5bd61cfdcd0e
Daviniel Nascimento <eng.daviniel@gmail.com>
2023-11-03 18:59:06 -0300

    corrigido o timer

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-import { secondsToTime } from "../utils/seconds-to-time";
+import React from 'react';
+import { secondsToMinutes } from '../utils/seconds-to-minutes';
 
 interface Props {
-  mainTime: number
+  mainTime: number;
 }
 
-export function Timer(props: Props): JSX.Element{
-  return (
-    <div className="timer">{secondsToTime(props.mainTime)}</div>
-  )
+export function Timer(props: Props): JSX.Element {
+  return <div className="timer">{secondsToMinutes(props.mainTime)}</div>;
 }
